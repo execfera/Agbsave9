2f228f7a51f230b60bc1c6ebb7e44d741cd2bb64
Alex Eckhart <eckhartalex@gmail.com>
2016-01-23 14:33:54 -0700

    Automate cia generation.

--- agb_inject/gen_rom.py
+++ agb_inject/gen_rom.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
+import struct
+import os
+savename = input("Type in the save's name: ")
+outname = input("Type in the output cia's name: ")
+savetype = int(input("Type in the save type: "))
+titleid = input("Type in the title id of the game you wish to inject: ")
+titleid = titleid[1:6]
+footername = "agb_inject_mb_" + str(savetype) + ".ftr"
+with open(footername, "rb") as f:
+    footer = f.read()
 with open("agb_inject_mb.gba", "rb") as f:
     rom = f.read()
-size = len(rom)
-size = 196608 - size
-rom = bytearray(rom + bytes(size))
-with open("inject.sav", "rb") as f:
+padding = struct.unpack("<i", footer[4:8])[0]
+padding -= len(rom)
+rom = bytearray(rom + bytes(padding) + footer)
+with open(savename, "rb") as f:
     sav = f.read()
-rom[0x1ffff] = 1
-rom[0x20000 : 0x20000 + 65536] = sav
-out = open("out.gba", "wb")
-out.write(rom)
-out.close()
+rom[0x1ffff] = savetype
+rom[0x20000 : 0x20000 + len(sav)] = sav
+with open("exefs/code.bin", "wb") as f:
+    f.write(rom)
+with open("agb_inject_mb.rsf", "rb") as f:
+    rsf = bytearray(f.read())
+rsf[0x180 : 0x185] = bytes(titleid, "ascii")
+with open("agb_inject_mb.rsf", "wb") as f:
+    f.write(rsf)
+os.system("./makerom -icon exefs/icon.bin " +
+"-banner exefs/banner.bin -code exefs/code.bin " +
+"-exheader exheader.bin -romfs romfs.bin -rsf agb_inject_mb.rsf -o gba.cxi")
